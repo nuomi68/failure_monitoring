f56078dcc686ba6f1bb066b7dcff2b45137ddbad
nuomi68 <92645412+nuomi68@users.noreply.github.com>
2025-08-05 19:39:30 +0800

    时间序列预测

--- backend/data_utils.py
+++ backend/data_utils.py
@@ -1,50 +1,28 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-from pandas.tseries.holiday import USFederalHolidayCalendar
+
 TIME_COL = "TIME"
 
 
-# def load_dataset(path: str):
-#     """Load and preprocess the dataset used by all models."""
-#     df = pd.read_excel(path)
-#     df.columns = df.columns.str.strip()
-#     df[TIME_COL] = pd.to_datetime(df[TIME_COL], format="%Y年%m月%d日%H%M")
-#     df = df.sort_values(TIME_COL).reset_index(drop=True)
-#
-#     features = (
-#         df.drop(columns=[TIME_COL, "值", "XE-133", "CS-137","KR-89"], errors="ignore")
-#         .apply(pd.to_numeric, errors="coerce")
-#         .ffill()
-#         .fillna(0)
-#     )
-#
-#     scaler = StandardScaler()
-#     features_scaled = scaler.fit_transform(features.astype(np.float32))
-#     return features_scaled, features.columns.tolist(), scaler
-
-VALUE_COLS_TO_DROP = ["值", "XE-133", "CS-137", "KR-89"]
-def load_dataset(path: str, log_transform=True):
-    """读取 Excel → 加时间特征 → log1p → 标准化"""
+def load_dataset(path: str):
+    """Load and preprocess the dataset used by all models."""
     df = pd.read_excel(path)
     df.columns = df.columns.str.strip()
     df[TIME_COL] = pd.to_datetime(df[TIME_COL], format="%Y年%m月%d日%H%M")
     df = df.sort_values(TIME_COL).reset_index(drop=True)
-    df['unique_id'] =  'multivariate'
-    time =  df[TIME_COL].copy()
-    # —— b) 数值特征
-    feats = df.drop( VALUE_COLS_TO_DROP, axis=1, errors="ignore")
-    feats = feats.apply(pd.to_numeric, errors="coerce").ffill().fillna(0)
 
-    # log1p 可稳定方差
-    if log_transform:
-        feats = feats.apply(np.log1p)
+    features = (
+        df.drop(columns=[TIME_COL, "值", "XE-133", "CS-137","KR-89"], errors="ignore")
+        .apply(pd.to_numeric, errors="coerce")
+        .ffill()
+        .fillna(0)
+    )
 
     scaler = StandardScaler()
-    feats_scaled = scaler.fit_transform(feats.astype(np.float32))
-    feats[TIME_COL] = time
-    feats['unique_id'] =  'multivariate'
-    return feats, feats.columns.tolist(), scaler
+    features_scaled = scaler.fit_transform(features.astype(np.float32))
+    return features_scaled, features.columns.tolist(), scaler
+
 
 def build_windows(data: np.ndarray, look_back: int):
     """Create sliding windows for sequence models."""
